94efc956635c308595419545f353f9ffa9e2228c
khurshidaalam <setukhurshidaalom@gmail.com>
2021-08-26 15:52:32 +0600

    new commmit

---
+++ src/components/Sdata.js
@@ -0,0 +1,23 @@
+const Sdata = [
+    {
+        id:1,
+        name:`product 1`,
+        model: `acsdfsdd`,
+        quality:`A`,
+        review:`*****`
+    },
+    {
+        id:2,
+        name:`product 2`,
+        model: `acsdfsdd`,
+        quality:`A`,
+        review:`*****`
+    },
+    {
+        id:3,
+        name:`product 3`,
+        model: `acsdfsdd`,
+        quality:`A`,
+        review:`*****`
+    }
+]
